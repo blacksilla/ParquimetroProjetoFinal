344ae5254feca4fb455565f1cdd2a608306def0b
Joao Teixeira <92153307+joaotx6@users.noreply.github.com>
2023-01-18 10:11:01 +0000

    Ticket class created

--- Parquimetro.cs
+++ Parquimetro.cs
@@ -36,6 +36,7 @@ namespace ParquimetroProjetoFinal
             Zonas Zona2 = new Zonas(2, 1, 7200000, MathHelper.returnRandomInt(1, 5 , 10));
             Zonas Zona3 = new Zonas(3, 0.62, 0, MathHelper.returnRandomInt(1, 5, 10));
 
+            Ticket.paymentNchange();
 
             while (menuMainActive) {
 
---
+++ Ticket.cs
@@ -0,0 +1,58 @@
+﻿using System;
+namespace ParquimetroProjetoFinal
+{
+	public class Ticket
+	{
+		public static void paymentNchange()
+		{
+			double saldo = 0;
+            double moeda;
+            bool state;
+
+			while (true)
+			{
+                Console.WriteLine("Insira a moeda.");
+                state = double.TryParse(Console.ReadLine(), out double readedInput);
+                if (state)
+                {
+                    moeda = readedInput;
+                }
+                else {
+                    moeda = 0;
+                }
+
+				switch (moeda)
+				{
+					case 0.05:
+						saldo = saldo + moeda;
+						break;
+                    case 0.1:
+                        saldo = saldo + moeda;
+                        break;
+                    case 0.2:
+                        saldo = saldo + moeda;
+                        break;
+                    case 0.5:
+                        saldo = saldo + moeda;
+                        break;
+                    case 1:
+                        saldo = saldo + moeda;
+                        break;
+                    case 2:
+                        saldo = saldo + moeda;
+                        break;
+
+                    default:
+                        Console.WriteLine("Moeda inválida.");
+						saldo = saldo;
+						break;
+				}
+
+				Console.WriteLine($"O saldo é {saldo}, e a moeda introduzida foi {moeda}");
+			}
+
+            Console.ReadLine();
+        }
+    }
+}
+
