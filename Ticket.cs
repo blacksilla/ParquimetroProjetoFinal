92e83aef06248eb8fb0eb39f3020a5583c3fc94e
blacksilla <afonsosilva1@ua.pt>
2023-01-27 10:58:00 +0000

    Ticket cases de muitos pra 1 line

--- Ticket.cs
+++ Ticket.cs
@@ -36,6 +36,7 @@ namespace ParquimetroProjetoFinal
             DateTime CurrentDate = DateTime.Now;
 
             double timeLimit = z.MaxTimeInMs;
+            bool paymentMenu = true;
 
             if (timeLimit == 0 && hour>9)
             {
@@ -51,116 +52,111 @@ namespace ParquimetroProjetoFinal
             }
             double pricePerHour = z.Preco;
             
-            Console.WriteLine($"O preço da Zona {z.Id} é {pricePerHour}€/hora e o tempo máximo de estacionamento são de {timeLimit} minutos");            
-
-			while (Math.Round(possibleParkingTime * 60, 1) <= timeLimit && wannaPay==false)
-			{
-
-                Console.WriteLine("Insira a moeda.");
-                state = double.TryParse(Console.ReadLine(), out double readedInput);
-                if (state)
-                {
-                    moeda = readedInput;
-                }
-                else {
-                    moeda = 0;
-                }
-
-				switch (moeda)
-				{
-					case 0.05:
-                        saldo = saldo + moeda;
-                        possibleParkingTime = saldo / pricePerHour;
-						break;
-                    case 0.1:
-                        saldo = saldo + moeda;
-
-                        possibleParkingTime = saldo / pricePerHour;
-                        break;
-                    case 0.2:
-                        saldo = saldo + moeda;
-
-                        possibleParkingTime = saldo / pricePerHour;
-                        break;
-                    case 0.5:
-                        saldo = saldo + moeda;
-
-                        possibleParkingTime = saldo / pricePerHour;
-                        break;
-                    case 1:
-                        saldo = saldo + moeda;
-
-                        possibleParkingTime = saldo / pricePerHour;
-                        break;
-                    case 2:
-                        saldo = saldo + moeda;
-
-                        possibleParkingTime = saldo / pricePerHour;
-                        break;
-
-                    default:
-                        Console.WriteLine("Moeda inválida.");
-						break;
-				}
-
-				Console.WriteLine($"O saldo é {saldo}€ e a moeda introduzida foi {moeda}€.");
-                
-                if(saldo > pricePerHour || possibleParkingTime*60>timeLimit)
-                {
-                    Console.WriteLine($"O tempo de estacionamento é {timeLimit} minutos");
-                    wannaPay = true;
-                }
-                else {
-                    Console.WriteLine($"O tempo de estacionamento é {Math.Round(possibleParkingTime * 60)} minutos");
-                    Console.WriteLine("Continuar - C || Pagar - P");
-                    var inputPayOrNot = Console.ReadLine();
-                    switch (inputPayOrNot)
-                    {
-                        case "P" or "p":
-                            wannaPay = true;
-                            break;
-
-                        case "C" or "c":
-                            wannaPay= false;
-                            break;
-                        default: InterfaceHelper.errorMessage(); wannaPay= false; break;
-                    }
-                }
-
-                
-            }
-            
-
-
-            Console.WriteLine("Confirmar Pagamento - C || Reiniciar - R");
-            
-            var input = Console.ReadLine();
-            switch (input)
+            Console.WriteLine($"O preço da Zona {z.Id} é {pricePerHour}€/hora e o tempo máximo de estacionamento são de {timeLimit} minutos");
+            while (paymentMenu)
             {
-                case "C" or "c":
-                    if (saldo > timeLimit / 60 * pricePerHour)
-                    {
-                        troco = Math.Round(saldo - (timeLimit / 60 * pricePerHour), 2);
-                        Console.WriteLine($"O troco é {troco}€");
-                        string license = getLicense();
-                        Ticket myticket = new Ticket(CurrentDate, z, license, saldo - troco,CurrentDate.AddMinutes(timeLimit));
-                        return myticket;
-                    }
-                    else //deverá ser elseif, ver melhor
-                    {
-                        troco = 0;
-                        string license = getLicense();
-                        Ticket myticket = new Ticket(CurrentDate, z, license, saldo - troco, CurrentDate.AddMinutes(timeLimit));
-                        return myticket;
-                    }
-                    
-                case "R" or "r":
-                    //reset das variáveis
-                    PaymentNchange(z,day,hour);
-                    break;
-                 default: InterfaceHelper.errorMessage(); break;
-            }
+                while (Math.Round(possibleParkingTime * 60, 1) <= timeLimit && wannaPay == false)
+                {
 
+                    Console.WriteLine("Insira a moeda.");
+                    state = double.TryParse(Console.ReadLine(), out double readedInput);
+                    if (state)
+                    {
+                        moeda = readedInput;
+                    }
+                    else
+                    {
+                        moeda = -1;
+                    }
+
+                    switch (moeda)
+                    {
+                        case 0.05:
+                        case 0.1:
+                        case 0.2:
+                        case 0.5:
+                        case 1:
+                        case 2:
+                            saldo = saldo + moeda;
+                            possibleParkingTime = saldo / pricePerHour;
+                            break;
+
+                        default:
+                            Console.WriteLine("Moeda inválida.");
+                            break;
+                    }
+                    if (moeda > 0)
+                    {
+                        Console.WriteLine($"O saldo é {saldo}€ e a moeda introduzida foi {moeda}€.");
+
+                        if (saldo > pricePerHour || possibleParkingTime * 60 > timeLimit)
+                        {
+                            Console.WriteLine($"O tempo de estacionamento é {timeLimit} minutos");
+                            wannaPay = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"O tempo de estacionamento é {Math.Round(possibleParkingTime * 60)} minutos");
+                            Console.WriteLine("Continuar - C || Pagar - P");
+                            var inputPayOrNot = Console.ReadLine();
+                            switch (inputPayOrNot)
+                            {
+                                case "P" or "p":
+                                    wannaPay = true;
+                                    break;
+
+                                case "C" or "c":
+                                    wannaPay = false;
+                                    break;
+                                default: InterfaceHelper.errorMessage(); wannaPay = false; continue;
+                            }
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Tente de novo");
+                    }
+                }
+
+
+
+                Console.WriteLine("Confirmar Pagamento - C || Reiniciar - R");
+
+                var input = Console.ReadLine();
+                switch (input)
+                {
+                    case "C" or "c":
+                        if (saldo > timeLimit / 60 * pricePerHour)
+                        {
+                            troco = Math.Round(saldo - (timeLimit / 60 * pricePerHour), 2);
+                            Console.WriteLine($"O troco é {troco}€");
+                            string license = getLicense();
+                            Ticket myticket = new Ticket(CurrentDate, z, license, saldo - troco, CurrentDate.AddMinutes(timeLimit));
+                            return myticket;
+                        }
+                        else //deverá ser elseif, ver melhor
+                        {
+                            troco = 0;
+                            string license = getLicense();
+                            Ticket myticket = new Ticket(CurrentDate, z, license, saldo - troco, CurrentDate.AddMinutes(timeLimit));
+                            return myticket;
+                        }
+
+                    case "R" or "r":
+                        //reset das variáveis
+                        PaymentNchange(z, day, hour);
+                        break;
+
+                    default: 
+                        saldo = 0;
+                        possibleParkingTime = 0;   
+                        wannaPay = false;
+                        InterfaceHelper.errorMessage();
+                        continue;
+                }
+
+            }
             return null;
         }
 
